8e214748eaa67e33df967f0dcb9b723439b4dd4f
moufmouf <d.negrier@thecodingmachine.com>
2021-08-24 14:39:16 +0000

    Deploying to gh-pages from master @ d1b8c957d046987bd2a495162ab44d33fc261788 🚀

---
+++ src/Components/Configs/YoutubeConfig.ts
@@ -0,0 +1,11 @@
+/**
+ * Declare and export the youtubeConfig interface
+ */
+
+export interface YoutubeConfig {
+    nameSpace : string,
+    channelID : string,
+    fullscreen : boolean,
+    autoplay: boolean,
+    pictureInPicture : boolean
+}
